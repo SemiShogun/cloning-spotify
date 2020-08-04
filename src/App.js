8e74e48ba2362abff7e7866a552b9b9237c3843e
NectoJ <nectoj@gmail.com>
2020-08-04 17:13:48 +0200

    Initialised Repo

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from './components/Login';
+import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './components/Player';
+import { useDataLayerValue, DataLayer } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [{ user, token }, dispatch ] = useDataLayerValue();
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then(playlists => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist('37i9dQZEVXcI7V6MZlUduE').then(playlist => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        })
+      })
+    }
+  }, []);
+
+  return (
+    // BEM Convention: Convention used for React
+    <div className="app">
+      { token ? <Player spotify={spotify} /> : <Login /> }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,31 @@
+// https://developer.spotify.com/
+// documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = 
+"https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "b91b6519b94448a3bd0b6071955761f8";
+
+// Gives the user the correct permissions
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            // #accessToken=mysupersecretkey&name=sonny
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+
+            return initial;
+        }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
